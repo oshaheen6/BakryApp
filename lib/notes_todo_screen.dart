a1c8cd58d28460c1f8775f717c54db1977c9577c
OsamaShaheen <os.shaheen6@gmail.com>
2024-11-13 16:06:41 +0200

    likes & comments in todo 1

--- lib/notes_todo_screen.dart
+++ lib/notes_todo_screen.dart
@@ -23,10 +23,10 @@ class NotesTodoScreen extends StatelessWidget {
               child: ListView(
                 children: [
                   _buildSectionTitle(context, 'Notes'),
-                  _buildNotesList(),
+                  _buildNotesList(context),
                   const SizedBox(height: 20),
                   _buildSectionTitle(context, 'Todos'),
-                  _buildTodosList(),
+                  _buildTodosList(context),
                 ],
               ),
             ),
@@ -57,7 +57,7 @@ class NotesTodoScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildNotesList() {
+  Widget _buildNotesList(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
       stream: _firestore
           .collection('patients')
@@ -77,31 +77,60 @@ class NotesTodoScreen extends StatelessWidget {
           itemCount: notes.length,
           itemBuilder: (context, index) {
             var note = notes[index];
-            return Card(
-              margin: const EdgeInsets.symmetric(vertical: 6.0),
-              elevation: 3,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-              child: ListTile(
-                title: Text(
-                  note['noteContent'],
-                  style: const TextStyle(
-                      fontSize: 16, fontWeight: FontWeight.w500),
-                ),
-                subtitle: Text(
-                  'Created on: ${note['dateCreated']}',
-                  style: TextStyle(color: Colors.grey[600]),
-                ),
-              ),
-            );
+            return _buildNoteCard(context, note);
           },
         );
       },
     );
   }
 
-  Widget _buildTodosList() {
+  Widget _buildNoteCard(BuildContext context, QueryDocumentSnapshot note) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 6.0),
+      elevation: 3,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              note['noteContent'], // Access the 'noteContent' field directly
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+            ),
+            Text(
+              'Created on: ${note['dateCreated']}',
+              style: TextStyle(color: Colors.grey[600]),
+            ),
+            const SizedBox(height: 8),
+            Row(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.thumb_up),
+                  color: Colors.blue,
+                  onPressed: () => _toggleLike(note),
+                ),
+                // Updated likes display to handle absence of 'likes' field
+                Text(
+                  '${(note.data() as Map<String, dynamic>)['likes'] ?? 0} likes',
+                ),
+                const Spacer(),
+                TextButton(
+                  child: const Text("Comments"),
+                  onPressed: () =>
+                      _showCommentsDialog(note.id, 'notes', context),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTodosList(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
       stream: _firestore
           .collection('patients')
@@ -121,29 +150,74 @@ class NotesTodoScreen extends StatelessWidget {
           itemCount: todos.length,
           itemBuilder: (context, index) {
             var todo = todos[index];
-            return Card(
-              margin: const EdgeInsets.symmetric(vertical: 6.0),
-              elevation: 3,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-              child: ListTile(
-                title: Text(
-                  todo['todoContent'],
-                  style: const TextStyle(
-                      fontSize: 16, fontWeight: FontWeight.w500),
-                ),
-                subtitle: Text(
-                  'Created on: ${todo['dateCreated']}\n'
-                  'Accomplished on: ${todo['dateAccomplished'] ?? 'Not yet accomplished'}',
-                  style: TextStyle(color: Colors.grey[600]),
-                ),
-              ),
-            );
+            return _buildTodoCard(context, todo);
           },
         );
       },
     );
+  }
+
+  Widget _buildTodoCard(BuildContext context, QueryDocumentSnapshot todo) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 6.0),
+      elevation: 3,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              todo['todoContent'],
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+            ),
+            Text(
+              'Created on: ${todo['dateCreated']}\n'
+              'Accomplished on: ${todo['dateAccomplished'] ?? 'Not yet accomplished'}',
+              style: TextStyle(color: Colors.grey[600]),
+            ),
+            const SizedBox(height: 8),
+            Row(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.thumb_up),
+                  color: Colors.blue,
+                  onPressed: () => _toggleLike(todo),
+                ),
+                // Using the null-aware operator to avoid 'containsKey'
+                Text(
+                  '${todo['likes'] ?? 0} likes',
+                ),
+                const Spacer(),
+                TextButton(
+                  child: const Text("Comments"),
+                  onPressed: () =>
+                      _showCommentsDialog(todo.id, 'todos', context),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _toggleLike(QueryDocumentSnapshot doc) async {
+    final docRef = _firestore
+        .collection('patients')
+        .doc(patientId)
+        .collection(doc.reference.parent.id)
+        .doc(doc.id);
+
+    await docRef.get().then((docSnapshot) {
+      if (docSnapshot.exists) {
+        final data = docSnapshot.data() as Map<String, dynamic>;
+        final likes = data['likes'] ?? 0; // Default to 0 if 'likes' is missing
+        docRef.update({'likes': likes + 1});
+      }
+    });
   }
 
   Widget _buildAddNoteButton(BuildContext context) {
@@ -224,18 +298,60 @@ class NotesTodoScreen extends StatelessWidget {
     );
   }
 
+  void _showCommentsDialog(
+      String docId, String collectionName, BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Comments'),
+        content: CommentsSection(
+            patientId: patientId, docId: docId, collectionName: collectionName),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('Close'),
+          ),
+        ],
+      ),
+    );
+  }
+
   void _addNoteToFirestore(String noteContent) {
     _firestore.collection('patients').doc(patientId).collection('notes').add({
       'noteContent': noteContent,
-      'dateCreated': DateTime.now().toString(),
+      'dateCreated': DateTime.now().toIso8601String(),
+      'likes': 0, // Ensure 'likes' field is always set
     });
   }
 
   void _addTodoToFirestore(String todoContent) {
     _firestore.collection('patients').doc(patientId).collection('todos').add({
       'todoContent': todoContent,
-      'dateCreated': DateTime.now().toString(),
-      'dateAccomplished': null,
+      'dateCreated': DateTime.now().toIso8601String(),
+      'likes': 0, // Ensure 'likes' field is always set
     });
+  }
+}
+
+class CommentsSection extends StatelessWidget {
+  final String patientId;
+  final String docId;
+  final String collectionName;
+
+  CommentsSection(
+      {required this.patientId,
+      required this.docId,
+      required this.collectionName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.maxFinite,
+      child: Column(
+        children: [
+          // Placeholder for comments list and input field
+        ],
+      ),
+    );
   }
 }
